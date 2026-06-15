3c6a27a8e265e9da231db263153583b3c3bb9c72
Yolo-TM <yolo.tm@outlook.de>
2026-06-15 18:56:49 +0200

    fix nix flake build

--- nix/default.nix
+++ nix/default.nix
@@ -1,5 +1,29 @@
-# Stub — the canonical package is defined in ../flake.nix.
-# This file previously copied pre-built binaries and is no longer used.
-# All builds go through rustPlatform.buildRustPackage in the flake.
-{ pkgs ? import <nixpkgs> {} }: builtins.throw
-  "Use the flake: nix build github:youruser/onedrive-mount or add to flake inputs."
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.stdenv.mkDerivation {
+  pname = "onedrive-mount";
+  version = "0.2.0";
+  src = ./.;
+
+  nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+
+  buildInputs = with pkgs; [
+    stdenv.cc.cc.lib
+    libx11
+    libxcursor
+    libxi
+    libxrandr
+    libxcb
+    libGL
+    libxkbcommon
+  ];
+
+  phases = [ "installPhase" ];
+  installPhase = ''
+    mkdir -p $out/bin $out/share/applications $out/share/icons/hicolor/scalable/apps
+    install -m755 $src/bin/onedrive-mount  $out/bin/
+    install -m755 $src/bin/onedrive-mountd $out/bin/
+    install -m644 $src/share/applications/onedrive-mount.desktop  $out/share/applications/
+    install -m644 $src/share/icons/hicolor/scalable/apps/onedrive-mount.svg $out/share/icons/hicolor/scalable/apps/
+  '';
+}
